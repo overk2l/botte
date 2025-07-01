b3e115ce4478a4b7a99d24f26a9d077739241ea5
overk2l <overk2ll1@gmail.com>
2025-07-01 18:51:55 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,26 +1,16 @@
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
-require('dotenv').config();
+require("dotenv").config();
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
 const commands = [
   new SlashCommandBuilder()
-    .setName('reactionroles')
-    .setDescription('Create a customizable reaction role message'),
-].map(command => command.toJSON());
+    .setName("dashboard")
+    .setDescription("Open the guild dashboard")
+    .toJSON()
+];
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands },
-    );
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
+const rest = new REST().setToken(process.env.TOKEN);
+rest.put(
+  Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+  { body: commands }
+).then(() => console.log("Commands deployed"))
+ .catch(console.error);
