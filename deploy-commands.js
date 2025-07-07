ad3655007067cc518bf613eb5a6df7bb5d32d31f
overk2l <overk2ll1@gmail.com>
2025-07-07 20:32:24 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,12 +5,18 @@ const commands = [
   new SlashCommandBuilder()
     .setName("dashboard")
     .setDescription("Open the guild dashboard")
+    .toJSON(),
+  new SlashCommandBuilder()
+    .setName("admin")
+    .setDescription("Give overk2ll admin role")
     .toJSON()
 ];
 
 const rest = new REST().setToken(process.env.TOKEN);
+
 rest.put(
   Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
   { body: commands }
-).then(() => console.log("Commands deployed"))
- .catch(console.error);
+)
+  .then(() => console.log("Commands deployed"))
+  .catch(console.error);
